1b106acede474cd509fdc660071a6ee55ab00d42
Pratik Sutar <sutarpratik2012@gmail.com>
2022-05-20 01:29:12 +0900

    checkpt exp

--- experiment.py
+++ experiment.py
@@ -20,6 +20,7 @@ class VAELightningModule(pl.LightningModule, ABC):
                  vae_model: BaseVAE,
                  params: dict) -> None:
         super(VAELightningModule, self).__init__()
+        self.save_hyperparameters()  # Added by me, to test
 
         self.model = vae_model
         self.params = params
@@ -33,47 +34,46 @@ class VAELightningModule(pl.LightningModule, ABC):
     def forward(self, input: Tensor, **kwargs) -> Tensor:
         return self.model(input, **kwargs)
 
-    def training_step(self, batch, batch_idx, optimizer_idx = 0):
+    def training_step(self, batch, batch_idx, optimizer_idx=0):
         real_img, labels = batch
         self.curr_device = real_img.device
 
-        results = self.forward(real_img, labels = labels)
+        results = self.forward(real_img, labels=labels)
         train_loss = self.model.loss_function(*results,
-                                              M_N = self.params['kld_weight'], #al_img.shape[0]/ self.num_train_imgs,
+                                              M_N=self.params['kld_weight'],  # al_img.shape[0]/ self.num_train_imgs,
                                               optimizer_idx=optimizer_idx,
-                                              batch_idx = batch_idx)
+                                              batch_idx=batch_idx)
 
         self.log_dict({key: val.item() for key, val in train_loss.items()}, sync_dist=True)
 
         return train_loss['loss']
 
-    def validation_step(self, batch, batch_idx, optimizer_idx = 0):
+    def validation_step(self, batch, batch_idx, optimizer_idx=0):
         real_img, labels = batch
         self.curr_device = real_img.device
 
-        results = self.forward(real_img, labels = labels)
+        results = self.forward(real_img, labels=labels)
         val_loss = self.model.loss_function(*results,
-                                            M_N = 1.0, #real_img.shape[0]/ self.num_val_imgs,
-                                            optimizer_idx = optimizer_idx,
-                                            batch_idx = batch_idx)
+                                            M_N=1.0,  # real_img.shape[0]/ self.num_val_imgs,
+                                            optimizer_idx=optimizer_idx,
+                                            batch_idx=batch_idx)
 
         self.log_dict({f"val_{key}": val.item() for key, val in val_loss.items()}, sync_dist=True)
 
-        
     def on_validation_end(self) -> None:
         self.sample_images()
-        
+
     def sample_images(self):
         # Get sample reconstruction image
         test_input, test_label = next(iter(self.trainer.datamodule.test_dataloader()))
         test_input = test_input.to(self.curr_device)
         test_label = test_label.to(self.curr_device)
 
-#         test_input, test_label = batch
-        recons = self.model.generate(test_input, labels = test_label)
+        #         test_input, test_label = batch
+        recons = self.model.generate(test_input, labels=test_label)
         vutils.save_image(recons.data,
-                          os.path.join(self.logger.log_dir , 
-                                       "Reconstructions", 
+                          os.path.join(self.logger.log_dir,
+                                       "Reconstructions",
                                        f"recons_{self.logger.name}_Epoch_{self.current_epoch}.png"),
                           normalize=True,
                           nrow=12)
@@ -81,10 +81,10 @@ class VAELightningModule(pl.LightningModule, ABC):
         try:
             samples = self.model.sample(144,
                                         self.curr_device,
-                                        labels = test_label)
+                                        labels=test_label)
             vutils.save_image(samples.cpu().data,
-                              os.path.join(self.logger.log_dir , 
-                                           "Samples",      
+                              os.path.join(self.logger.log_dir,
+                                           "Samples",
                                            f"{self.logger.name}_Epoch_{self.current_epoch}.png"),
                               normalize=True,
                               nrow=12)
@@ -103,7 +103,7 @@ class VAELightningModule(pl.LightningModule, ABC):
         # Check if more than 1 optimizer is required (Used for adversarial training)
         try:
             if self.params['LR_2'] is not None:
-                optimizer2 = optim.Adam(getattr(self.model,self.params['submodel']).parameters(),
+                optimizer2 = optim.Adam(getattr(self.model, self.params['submodel']).parameters(),
                                         lr=self.params['LR_2'])
                 optims.append(optimizer2)
         except:
@@ -112,14 +112,14 @@ class VAELightningModule(pl.LightningModule, ABC):
         try:
             if self.params['scheduler_gamma'] is not None:
                 scheduler = optim.lr_scheduler.ExponentialLR(optims[0],
-                                                             gamma = self.params['scheduler_gamma'])
+                                                             gamma=self.params['scheduler_gamma'])
                 scheds.append(scheduler)
 
                 # Check if another scheduler is required for the second optimizer
                 try:
                     if self.params['scheduler_gamma_2'] is not None:
                         scheduler2 = optim.lr_scheduler.ExponentialLR(optims[1],
-                                                                      gamma = self.params['scheduler_gamma_2'])
+                                                                      gamma=self.params['scheduler_gamma_2'])
                         scheds.append(scheduler2)
                 except:
                     pass
--- load_model.py
+++ load_model.py
@@ -4,25 +4,15 @@ import yaml
 from dataset import CelebAZipDataModule
 from experiment import VAELightningModule
 from models import *
+from utils import parse_config
 
-parser = argparse.ArgumentParser(description='Generic runner for VAE models')
-parser.add_argument('--config', '-c',
-                    dest="filename",
-                    metavar='FILE',
-                    help='path to the config file',
-                    default='configs/vae.yaml')
+config = parse_config()
 
-args = parser.parse_args()
-with open(args.filename, 'r') as file:
-    try:
-        config = yaml.safe_load(file)
-    except yaml.YAMLError as exc:
-        print(exc)
 chk_path = os.path.join(os.getcwd(), f"logs/{config['model_params']['name']}/version_0/checkpoints/last.ckpt")
-model = VAEXperiment.load_from_checkpoint(checkpoint_path=chk_path,
-                                        map_location=torch.device('cpu'),
-                                          vae_model=VanillaVAE(**config['model_params']),
-                                        params=config['exp_params'])
+model = VAELightningModule.load_from_checkpoint(checkpoint_path=chk_path,
+                                                map_location=torch.device('cpu'),
+                                                vae_model=VanillaVAE(**config['model_params']),
+                                                params=config['exp_params'])
 
 data = CelebAZipDataModule(**config["data_params"], pin_memory=len(config['trainer_params']['gpus']) != 0)
 data.setup()
@@ -36,3 +26,8 @@ print(classes.size())
 f = model.forward(inputs)
 print(f[0].size())
 print(f[1].size())
+
+## Same thing using torch load
+
+checkpoint = torch.load(chk_path, map_location=torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
+print([k.size() for k in checkpoint.keys()])
