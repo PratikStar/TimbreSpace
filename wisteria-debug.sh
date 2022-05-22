5e268c0169d66a220d747e708b1cd1c9766eb071
Pratik Sutar <sutarpratik2012@gmail.com>
2022-05-22 16:42:35 +0900

    live updates
    Former-commit-id: e7ec148cc9c782f647fff269b23bc53f68413f71

--- wisteria-debug.sh
+++ wisteria-debug.sh
@@ -2,10 +2,10 @@
 #PJM -g gk77
 #PJM -L rscgrp=share-debug
 #PJM -L gpu=4
-#PJM -N Pytorch-VAE
+#PJM -N Pytorch-VQVAE
 #PJM -j
 #PJM -m b
 #PJM -m e
 
 # run commands
-python3 run-vae.py -c configs/vae-gpu.yaml
+python3 run-vae.py -c configs/vq_vae-gpu.yaml
