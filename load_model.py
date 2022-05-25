2dbf2abbc19052bd6dc842cde0a55521f68e184c
Pratik Sutar <sutarpratik2012@gmail.com>
2022-05-25 21:38:14 +0900

    something update

--- load_model.py
+++ load_model.py
@@ -1,19 +1,18 @@
-import argparse
 import os
 import yaml
 from dataset import CelebAZipDataModule
 from experiment import VAELightningModule
 from models import *
-from utils import parse_config
-import torchvision.utils as vutils
-import numpy as np
+from utils import *
 
-config = parse_config()
+# config = get_config(parse_args())
 
-chk_path = os.path.join(os.getcwd(), f"logs/{config['model_params']['name']}/version_0/checkpoints/last.ckpt")
+config = get_config(os.path.join(os.getcwd(), 'configs/vq_vae-gpu.yaml'))
+
+chk_path = os.path.join(os.getcwd(), f"logs/{config['model_params']['name']}/version_4/checkpoints/last.ckpt")
 model = VAELightningModule.load_from_checkpoint(checkpoint_path=chk_path,
                                                 map_location=torch.device('cpu'),
-                                                vae_model=VanillaVAE(**config['model_params']),
+                                                vae_model= vae_models[config['model_params']['name']](**config['model_params']),
                                                 params=config['exp_params'])
 # Same thing using torch load
 # checkpoint = torch.load(chk_path, map_location=torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
@@ -25,16 +24,17 @@ dl = data.train_dataloader()
 inputs, classes = next(iter(dl))
 f = model.forward(inputs)
 
-z = torch.randn(128)
-s = model.model.decode(z)
-
-vutils.save_image(s, f'sliding/sample-OG.png', normalize=True)
-for dim in range(0, 128):
-    arr = []
-    for i in np.arange(-10, 10, 1):
-        z[dim] = i
-        arr.append(model.model.decode(z))
-    res = torch.cat(arr, dim=0)
-    vutils.save_image(res, f'sliding/sample-{dim}.png', normalize=True, nrow=20)
+#
+# z = torch.randn(128)
+# s = model.model.decode(z)
+#
+# vutils.save_image(s, f'sliding/sample-OG.png', normalize=True)
+# for dim in range(0, 128):
+#     arr = []
+#     for i in np.arange(-10, 10, 1):
+#         z[dim] = i
+#         arr.append(model.model.decode(z))
+#     res = torch.cat(arr, dim=0)
+#     vutils.save_image(res, f'sliding/sample-{dim}.png', normalize=True, nrow=20)
 
 
