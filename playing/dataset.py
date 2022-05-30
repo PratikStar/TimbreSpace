f1d85f6c7b35bc0b72a9f44ea6ec6ffc4d5cb524
Pratik Sutar <sutarpratik2012@gmail.com>
2022-05-31 00:58:08 +0900

    debugging

--- playing/dataset.py
+++ playing/dataset.py
@@ -36,10 +36,13 @@ class CelebAZipDataset(VisionDataset):
 
         mask = slice(None)
         attr = self._load_csv(os.path.join(self.root_path, "list_attr_celeba.txt"), header=1)
-        print(attr)
+        # print(attr)
+        print(attr[0])
         self.attr = attr.data[mask]
-        print(self.attr)
+        print(self.attr.size())
+        print(self.attr[0])
         self.attr = torch.div(self.attr + 1, 2, rounding_mode="floor")
+        print(self.attr.size())
         print(self.attr)
 
     def _load_csv(
