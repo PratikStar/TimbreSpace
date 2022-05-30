5c62fb01d4ea7d3ccfc5e9903bfc591bf801b0a9
Pratik Sutar <sutarpratik2012@gmail.com>
2022-05-31 02:08:34 +0900

    debugging

--- playing/dataset.py
+++ playing/dataset.py
@@ -32,9 +32,9 @@ class CelebAZipDataset(VisionDataset):
         else:
             self.zip_file = zipfile.ZipFile(zip_file_path, 'r')
 
-        self.name_list = list(filter(lambda x: x[-4:] == '.jpg', self.zip_file.namelist()))
         print(self.name_list[:10])
         self.datadict = self._load_csv(os.path.join(self.root_path, "list_attr_celeba.txt"), attribute, header=1)
+        self.name_list = ['' for x, y in self.datadict]
 
         print(self.datadict[:10])
         print(len(self.datadict))
