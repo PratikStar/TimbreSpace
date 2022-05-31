856aae73c6551b3c2c01e22f7589a215ca5aff4d
Pratik Sutar <sutarpratik2012@gmail.com>
2022-06-01 02:00:05 +0900

    debugging
    Former-commit-id: 222258931d1492ca770ce7b0235ea822217eec6c

--- male-female-embeddings.py
+++ male-female-embeddings.py
@@ -1,9 +1,11 @@
 import csv
-
+import numpy as np
 # Read csv
 with open('male.csv', newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    a = np.array((128,))
     for row in csvreader:
-        em = row[0].split(',')
+        em = [float(i) for i in row[0].split(',')]
+        np.insert(a, em)
         print(', '.join(row))
         break
