2757368e66e6745b2b05d974533f234700d849d0
Pratik Sutar <sutarpratik2012@gmail.com>
2022-06-01 00:44:00 +0900

    debugging
    Former-commit-id: 87a12d9bb96d69cbd641a1d45692bf987860ad4f

--- load_model.py
+++ load_model.py
@@ -1,10 +1,15 @@
 import os
 import yaml
+from torch.utils.data import DataLoader
+
 from dataset import CelebAZipDataModule
 from experiment import VAELightningModule
 from models import *
+from playing.dataset import CelebAZipDatasetWithFilter
 from utils import *
 import csv
+from torchvision import transforms
+
 
 config = get_config(os.path.join(os.getcwd(), 'configs/vae.yaml'))
 
@@ -14,12 +19,12 @@ model = VAELightningModule.load_from_checkpoint(checkpoint_path=chk_path,
                                                 vae_model= vae_models[config['model_params']['name']](**config['model_params']),
                                                 params=config['exp_params'])
 
-data = CelebAZipDataModule(**config["data_params"], pin_memory=len(config['trainer_params']['gpus']) != 0)
-data.setup()
-dl = data.train_dataloader()
-inputs, classes = next(iter(dl))
+## For generating embeddings
+# data = CelebAZipDataModule(**config["data_params"], pin_memory=len(config['trainer_params']['gpus']) != 0)
+# data.setup()
+# dl = data.train_dataloader()
+# inputs, classes = next(iter(dl))
 
-# For generating embeddings
 # times = 50
 # for step, (inputs, classes) in enumerate(dl):
 #     if step == times:
@@ -37,3 +42,20 @@ inputs, classes = next(iter(dl))
 
 
 
+train_transforms = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                       transforms.CenterCrop(148),
+                                       transforms.Resize(64),
+                                       transforms.ToTensor(), ])
+ds = CelebAZipDatasetWithFilter('../../data/celeba', ('Male', 1),
+                                transform=train_transforms)
+
+dl = DataLoader(
+    ds,
+    batch_size=64,
+    num_workers=0,
+    shuffle=False,
+    pin_memory=False,
+)
+
+iterdl = iter(dl)
+x, y, k = next(iterdl)
