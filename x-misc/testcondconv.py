5f58a55c0f8c0875172e58afb39752525ae7e3ef
Pratik Sutar <sutarpratik2012@gmail.com>
2022-08-25 12:23:37 +0900

    online development

--- x-misc/testcondconv.py
+++ x-misc/testcondconv.py
@@ -6,7 +6,7 @@ from torch import nn
 class Model(nn.Module):
     def __init__(self, num_experts):
         super(Model, self).__init__()
-        self.condconv2d = CondConv2D(in_channels=1, out_channels=128, kernel_size=3, num_experts=num_experts, padding=1)
+        self.condconv2d = CondConv2D(in_channels=1, out_channels=32, kernel_size=3, num_experts=num_experts, padding=1)
 
     def forward(self, x):
         x = self.condconv2d(x)
